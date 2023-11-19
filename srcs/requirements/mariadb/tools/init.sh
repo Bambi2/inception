257bc4e684b400b973dc003b5eb8d751102da45a
Evgenii GRIGORIAN <mmalphit@made-f0Br7s5.clusters.42paris.fr>
2023-11-19 20:03:08 +0100

    wip

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,19 +1,19 @@
-#!bin/sh
+#!/bin/bash
 #set -eux
 
-# sql commands to create user, update adming password and create wordpress database
-cat << EOF > /tmp/init.sql
-USE mysql;
-FLUSH PRIVILEGES;
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE test;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}';
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER '${USERNAME}'@'%' IDENTIFIED by '${MARIADB_USER_PWD}';
-GRANT ALL PRIVILEGES ON wordpress.* TO  '${USERNAME}'@'%';
-FLUSH PRIVILEGES;
-EOF
+service mysql start;
 
-# run sql script
-/usr/bin/mysqld --user=mysql --bootstrap < /tmp/init.sql
-rm -f /tmp/init.sql
+# log into MariaDB as root and create database and the user
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${USERNAME}\`@'localhost' IDENTIFIED BY '${MARIADB_USER_PWD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${USERNAME}\`@'%' IDENTIFIED BY '${MARIADB_USER_PWD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p${MARIADB_ROOT_PWD} shutdown
+
+#mysqladmin -u root shutdown
+exec mysqld_safe
+
+#print status
+echo "MariaDB database and user were created successfully! "
