60a376ef0a616c9c711fda1b342d80b5b5f90953
Evgenii GRIGORIAN <mmalphit@made-f0Br8s16.clusters.42paris.fr>
2023-11-21 20:37:00 +0100

    asd

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,37 +1,19 @@
 #!bin/sh
-
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-
-        chown -R mysql:mysql /var/lib/mysql
-
-        # init database
-        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-
-        tfile=`mktemp`
-        if [ ! -f "$tfile" ]; then
-                return 1
-        fi
-fi
-
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
+#set -eux
 
 # sql commands to create user, update adming password and create wordpress database
+# TODO: add permissions of wordpress db to username
 cat << EOF > /tmp/init.sql
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}';
-CREATE DATABASE ${MARIADB_DATABASE};
+CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER '${USERNAME}'@'%' IDENTIFIED by '${MARIADB_USER_PWD}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${USERNAME}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
 # run sql script
 /usr/bin/mysqld --user=mysql --bootstrap < /tmp/init.sql
 rm -f /tmp/init.sql
-
-fi
