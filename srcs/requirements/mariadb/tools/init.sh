8e53b5019e3bd6e483daa89194613cdaa0770ed7
Evgenii GRIGORIAN <mmalphit@made-f0Br8s16.clusters.42paris.fr>
2023-11-21 20:08:46 +0100

    try

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -8,6 +8,8 @@ USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}';
 CREATE DATABASE ${MARIADB_DATABASE};
 CREATE USER '${USERNAME}'@'%' IDENTIFIED by '${MARIADB_USER_PWD}';
